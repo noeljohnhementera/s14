5ae7c1c36266fbbccc89551d00eaafb768890970
noeljohnhementera97 <noeljohnhementera97@gmail.com>
2021-08-23 21:42:46 +0800

    Added JavaScript activity #1

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = prompt('Enter Your Name:')
+console.log(name);
+
+let email = prompt('Enter Your Email:')
+console.log(email);
+
+let phonenumber = prompt('Enter Your Phone Number:')
+console.log(phonenumber);
